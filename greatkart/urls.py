5b6f458cd3d027b9bd2dc0540edf101ac23d5676
Alzuuu <alazarteshite96@gmail.com>
2022-02-03 21:01:39 +0300

    storeand carts functionality

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name="home"),
     path('store/', include('store.urls')),
-    path('cart/', include('carts.urls'))
+    path('cart/', include('carts.urls')),
+    path('account/',include('account.urls'))
    # path('base/',views.base,name="base"),
 ] + static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
