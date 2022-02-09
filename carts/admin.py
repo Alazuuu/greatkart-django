2a1b05818b917a81e8cecbbc833706f85f06086c
Alzuuu <alazarteshite96@gmail.com>
2022-02-09 23:51:34 +0300

    handeled cart and checkout page for the logged in user

--- carts/admin.py
+++ carts/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-from .models import Cart,CartItem
-# Register your models here.
-class CartAdmin(admin.ModelAdmin):
-    list_display= {'cart_id','date_added'}
 
+from . import models
+
+@admin.register(models.Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display=('cart_id','date_added',)
+
+
+@admin.register(models.CartItem)
 class CartItemAdmin(admin.ModelAdmin):
-    list_display = {'product', 'cart','quanlity','is_active'}
-    
-admin.site.register(Cart)
-admin.site.register(CartItem)
+    list_display = ('product','cart','quantity','is_active',)
+
+
