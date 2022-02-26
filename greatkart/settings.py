e1cab4d3b192f5f1e5ad4961a1901a8fa824824a
Alazuuu <90863833+Alazuuu@users.noreply.github.com>
2022-02-26 23:50:59 +0300

    first deploye

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default = True, cast=bool)
+DEBUG = config('DEBUG', default = False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ale-commerce.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- greatkart/urls.py
+++ greatkart/urls.py
@@ -19,6 +19,9 @@ from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
     path('securelogin/', admin.site.urls),
@@ -27,5 +30,7 @@ urlpatterns = [
     path('cart/', include('carts.urls')),
     path('accounts/',include('accounts.urls')),
     path('orders/', include('orders.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
    # path('base/',views.base,name="base"),
 ] + static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
